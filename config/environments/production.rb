d242fe4a640a31c2f81e217f60dfda48fd6e5f0f
Brahyam Meneses <meneses.brahyam@gmail.com>
2016-07-21 09:28:15 -0400

    fixes for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Enable stdout logger
+  config.logger = Logger.new(STDOUT)
 end
